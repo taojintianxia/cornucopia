b3e1768c15447b6bad6369666a389d02aa30bee3
Kane.Sun <taojintianxia@126.com>
2013-09-26 09:42:55 +0800

    add new feature for hashCode method

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,6 @@
-import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.Random;
 
 /**
@@ -12,10 +12,44 @@ import java.util.Random;
  */
 public class Test {
 
-	public static void main(String... args) {
-		String str = "万业企业 600641";
-		System.out.println(str.substring(str.lastIndexOf(" ") + 1));
-		System.out.println(str.substring(0,str.lastIndexOf(" ")));
+	private static Random random = new Random();
+
+	public static void main(String... args) throws Exception {
+		for(int i = 0 ; i < 3;i++){
+			if(i==2)
+				continue;
+			System.out.println("------");
+		}
 	}
 
+	public static void getMaxmiumSubSum(int[] targetArray) {
+		int length = targetArray.length;
+		int[] minusArray = new int[length - 1];
+		for (int i = 0; i < length - 2; i++) {
+			minusArray[i] = targetArray[i] - targetArray[i + 1];
+		}
+		int sum = 0;
+		int preIndex = 0;
+		int nextIndex = 0;
+		int max = 0;
+
+		for (int i = 0; i < length - 2; i++) {
+			sum += minusArray[i];
+			if (sum > 0) {
+				sum = 0;
+			} else {
+				if (sum == minusArray[i]) {
+					preIndex = i;
+				}
+			}
+
+			if (max > sum) {
+				max = sum;
+				nextIndex = i;
+			}
+		}
+
+		System.out.println("first Number : " + targetArray[preIndex]);
+		System.out.println("second Number : " + targetArray[nextIndex + 1]);
+	}
 }
