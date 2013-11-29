9aa12629ce0be8447f1e67c940ed6f4d7dd54649
Kane.Sun <taojintianxia@126.com>
2013-11-29 18:55:06 +0800

    add a method to detect the memory of JVM

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,3 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
 
 /**
  * 
@@ -13,61 +7,22 @@ import java.sql.Statement;
  * 
  */
 public class Test {
-
+	
 	public static void main(String... args) throws Exception {
-		Connection conn = null;
-		
-		try {
-		    conn =
-		       DriverManager.getConnection("jdbc:mysql://localhost/rockblade?" +
-		                                   "user=root&password=root");
-		    // Do something with the Connection
-		   
-		} catch (SQLException ex) {
-		    // handle any errors
-		    System.out.println("SQLException: " + ex.getMessage());
-		    System.out.println("SQLState: " + ex.getSQLState());
-		    System.out.println("VendorError: " + ex.getErrorCode());
-		}
+		Runtime run = Runtime.getRuntime(); 
 
-		// assume that conn is an already created JDBC connection (see previous examples)
-		Statement stmt = null;
-		ResultSet rs = null;
-		try {
-		    stmt = conn.createStatement();
-		    rs = stmt.executeQuery("SELECT * FROM stockdetail");
-		    // or alternatively, if you don't know ahead of time that
-		    // the query will be a SELECT...
-		    while(rs.next())
-		    {
-		    int result=rs.getInt("stockid");
-		    System.out.println(result);
-		    }
-		}
-		catch (SQLException ex){
-		    // handle any errors
-		    System.out.println("SQLException: " + ex.getMessage());
-		    System.out.println("SQLState: " + ex.getSQLState());
-		    System.out.println("VendorError: " + ex.getErrorCode());
-		}
-		finally {
-		    // it is a good idea to release
-		    // resources in a finally{} block
-		    // in reverse-order of their creation
-		    // if they are no-longer needed
-		    if (rs != null) {
-		        try {
-		            rs.close();
-		        } catch (SQLException sqlEx) { } // ignore
-		        rs = null;
-		    }
-		    if (stmt != null) {
-		        try {
-		            stmt.close();
-		        } catch (SQLException sqlEx) { } // ignore
-		        stmt = null;
-		    }
-		}
+		long max = run.maxMemory(); 
+
+		long total = run.totalMemory(); 
+
+		long free = run.freeMemory(); 
+
+		long usable = max - total + free; 
+
+		System.out.println("最大内存 = " + max); 
+		System.out.println("已分配内存 = " + total); 
+		System.out.println("已分配内存中的剩余空间 = " + free); 
+		System.out.println("最大可用内存 = " + usable); 
 	}
-
+	
 }
