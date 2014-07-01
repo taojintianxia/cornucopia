7547d487360133c7f94932852ff1d9487f5d8a15
Kane.Sun <taojintianxia@126.com>
2014-07-01 08:41:09 +0800

    add new features

---
+++ src/pattern/IfElseRefactory.java
@@ -0,0 +1,129 @@
+package pattern;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ *
+ * @author Kane.Sun
+ * @version Jun 10, 2014 1:55:11 PM
+ * 
+ */
+@SuppressWarnings("unused")
+public class IfElseRefactory {
+
+	public static void main(String... args) throws Exception {
+		IfElseRefactory ifElseRefactory = new IfElseRefactory();
+		enumSolution("A");
+		enumSolution("B");
+		dynamicInvokeSolution("C", ifElseRefactory);
+	}
+
+	public static void basicSolution(String targetStr) {
+		if (targetStr.equals("A")) {
+			System.out.println(targetStr + " is printing ");
+		} else if (targetStr.equals("B")) {
+			System.out.println(targetStr + " is printing ");
+		} else if (targetStr.equals("C")) {
+			System.out.println(targetStr + " is printing ");
+		} else if (targetStr.equals("D")) {
+			System.out.println(targetStr + " is printing ");
+		} else {
+			System.out.println("Other" + " is printing ");
+		}
+	}
+
+	public static void enumSolution(String str) {
+		StrEnum.getInstance(str).handleStr();
+	}
+
+	private enum StrEnum {
+
+		StrA("A") {
+			@Override
+			public void handleStr() {
+				System.out.println(getStr() + " is printing ");
+			}
+		},
+		StrB("B") {
+			@Override
+			public void handleStr() {
+				System.out.println(getStr() + " is printing ");
+			}
+		},
+		StrC("C") {
+			@Override
+			public void handleStr() {
+				System.out.println(getStr() + " is printing ");
+			}
+		},
+		StrD("D") {
+			@Override
+			public void handleStr() {
+				System.out.println(getStr() + " is printing ");
+			}
+		},
+		Other("Other") {
+			@Override
+			public void handleStr() {
+				System.out.println(getStr() + " is printing ");
+			}
+		};
+
+		private String str;
+
+		public String getStr() {
+			return this.str;
+		}
+
+		public abstract void handleStr();
+
+		private StrEnum(String str) {
+			this.str = str;
+		}
+
+		public static StrEnum getInstance(String str) {
+			for (StrEnum strEnum : StrEnum.values()) {
+				if (strEnum.str.equals(str)) {
+					return strEnum;
+				}
+			}
+			return Other;
+		}
+	}
+
+	public static void dynamicInvokeSolution(String str, IfElseRefactory ifElseRefactory) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException,
+			SecurityException {
+		Map<String, Method> testMap = new HashMap<>();
+		testMap.put("A", IfElseRefactory.class.getDeclaredMethod("handleA"));
+		testMap.put("B", IfElseRefactory.class.getDeclaredMethod("handleB"));
+		testMap.put("C", IfElseRefactory.class.getDeclaredMethod("handleC"));
+		testMap.put("D", IfElseRefactory.class.getDeclaredMethod("handleD"));
+		testMap.put("Other", IfElseRefactory.class.getDeclaredMethod("handleOther"));
+		testMap.get(str).invoke(ifElseRefactory);
+	}
+
+	private void handleA() {
+		System.out.println("A is printing ");
+	}
+
+	private void handleB() {
+
+		System.out.println("B is printing ");
+	}
+
+	private void handleC() {
+		System.out.println("C is printing ");
+	}
+
+	private void handleD() {
+		System.out.println("D is printing ");
+	}
+
+	private void handleOther() {
+		System.out.println("Other is printing ");
+	}
+}
