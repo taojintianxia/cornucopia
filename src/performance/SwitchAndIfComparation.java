4c365cd8432f256bf62bd2f93ca43a4238f76ae9
Kane.Sun <taojintianxia@126.com>
2014-09-16 17:05:32 +0800

    add test to compare the performance between if else keyword and switch case

---
+++ src/performance/SwitchAndIfComparation.java
@@ -0,0 +1,51 @@
+package performance;
+
+import java.util.Random;
+
+public class SwitchAndIfComparation {
+
+	private Random ramdom = new Random();
+	private int ix = 3;
+
+	public static void main(String... args) {
+		SwitchAndIfComparation comparationTest = new SwitchAndIfComparation();
+		long switchStart = System.nanoTime();
+		for (int i = 0; i < 10000; i++) {
+			comparationTest.switchTest();
+		}
+
+		long switchEnd = System.nanoTime();
+		long ifElsestart = System.nanoTime();
+		for (int i = 0; i < 10000; i++) {
+			comparationTest.testIfElse();
+		}
+		long ifElseEnd = System.nanoTime();
+
+		System.out.println("Switch KeyWord takes " + (switchEnd - switchStart));
+		System.out.println("If Else KeyWord takes " + (ifElseEnd - ifElsestart));
+	}
+
+	public void switchTest() {
+		int i = ramdom.nextInt(3);
+		switch (i) {
+		case 0:
+			System.out.println("0");
+		case 1:
+			System.out.println("1");
+		case 2:
+			System.out.println(2);
+		}
+
+	}
+
+	public void testIfElse() {
+		int i = ramdom.nextInt(3);
+		if (i == 0) {
+			System.out.println("0");
+		} else if (i == 1) {
+			System.out.println("1");
+		} else if (i == 2) {
+			System.out.println("2");
+		}
+	}
+}
