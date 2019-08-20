34db1b8c228249bf09a648e3d04f6c464dfcc6ab
sunnianjun <sunnianjun@huobi.com>
2019-08-20 19:00:21 +0800

    Add: add controller

---
+++ configuration/nacos/nacos-springboot-example/src/main/java/com/github/taojintianxia/nacos/springboot/controller/SchoolController.java
@@ -0,0 +1,38 @@
+package com.github.taojintianxia.nacos.springboot.controller;
+
+import com.github.taojintianxia.nacos.springboot.config.ClassRoomConfig;
+import com.github.taojintianxia.nacos.springboot.config.SchoolConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+/**
+ * @author Nianjun Sun
+ * @date 2019-08-20 18:53
+ */
+@Controller
+@RequestMapping("school")
+public class SchoolController {
+
+    @Autowired
+    private ClassRoomConfig classRoomConfig;
+
+    @Autowired
+    private SchoolConfig schoolConfig;
+
+    @ResponseBody
+    @RequestMapping(value = "/getSchool", method = GET)
+    public String getSchool() {
+        return "学校面积 : " + schoolConfig.getArea() + "\n学校等级 : " + schoolConfig.getLevel();
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/getClassroom", method = GET)
+    public String getClassroom() {
+        return "教室面积 : " + classRoomConfig.getArea() + "\n教室座椅数量 : " + classRoomConfig.getChairNumber() + "\n是否是老教室 : "
+                + classRoomConfig.isLegacy();
+    }
+}
