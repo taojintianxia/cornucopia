baee6aafc31d382e21ce900e2971dca1803ad034
sunnianjun <sunnianjun@huobi.com>
2019-08-29 17:41:53 +0800

    Bump: add new dependency

---
+++ shardingsphere-example/sharding-common/src/main/java/com/github/taojintianxia/cornucopia/shardingcommon/entity/OrderItem.java
@@ -0,0 +1,16 @@
+package com.github.taojintianxia.cornucopia.shardingcommon.entity;
+
+/**
+ * @author Nianjun Sun
+ * @date 2019/8/29 17:39
+ */
+public class OrderItem {
+
+    private long orderItemId;
+
+    private long orderId;
+
+    private int userId;
+
+    private String status;
+}
---
+++ shardingsphere-example/sharding-common/src/main/java/com/github/taojintianxia/cornucopia/shardingcommon/entity/User.java
@@ -0,0 +1,8 @@
+package com.github.taojintianxia.cornucopia.shardingcommon.entity;
+
+/**
+ * @author Nianjun Sun
+ * @date 2019/8/29 17:38
+ */
+public class User {
+}
