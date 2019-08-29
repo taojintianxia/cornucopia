8e4587dc273c3288fdd8136650885d5ac4fb8661
Kane.Sun <kane.sun@outlook.com>
2019-08-29 21:08:26 +0800

    Add: add entity

--- shardingsphere-example/sharding-common/src/main/java/com/github/taojintianxia/cornucopia/shardingcommon/entity/OrderItem.java
+++ shardingsphere-example/sharding-common/src/main/java/com/github/taojintianxia/cornucopia/shardingcommon/entity/OrderItem.java
@@ -1,10 +1,17 @@
 package com.github.taojintianxia.cornucopia.shardingcommon.entity;
 
+import lombok.Data;
+
+import java.io.Serializable;
+
 /**
  * @author Nianjun Sun
  * @date 2019/8/29 17:39
  */
-public class OrderItem {
+@Data
+public class OrderItem implements Serializable {
+
+    private static final long serialVersionUID = 8430520334443007174L;
 
     private long orderItemId;
 
--- shardingsphere-example/sharding-common/src/main/java/com/github/taojintianxia/cornucopia/shardingcommon/entity/User.java
+++ shardingsphere-example/sharding-common/src/main/java/com/github/taojintianxia/cornucopia/shardingcommon/entity/User.java
@@ -1,8 +1,25 @@
 package com.github.taojintianxia.cornucopia.shardingcommon.entity;
 
+import lombok.Data;
+
+import java.io.Serializable;
+
 /**
  * @author Nianjun Sun
  * @date 2019/8/29 17:38
  */
-public class User {
+@Data
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 5915686070467656424L;
+
+    private int userId;
+
+    private String userName;
+
+    private String userNamePlain;
+
+    private String pwd;
+
+    private String assistedQueryPwd;
 }
