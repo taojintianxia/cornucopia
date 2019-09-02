1a961073db757f9322630ff99eb4b2e79e3a7565
sunnianjun <sunnianjun@huobi.com>
2019-09-02 10:47:12 +0800

    Add: add new dependency

--- shardingsphere-example/sharding-jdbc-example/src/main/java/com/github/taojintianxia/shardingsphere/shardingjdbcexample/rawjdbc/masterslave/MasterSlaveApplication.java
+++ shardingsphere-example/sharding-jdbc-example/src/main/java/com/github/taojintianxia/shardingsphere/shardingjdbcexample/rawjdbc/masterslave/MasterSlaveApplication.java
@@ -1,8 +1,17 @@
 package com.github.taojintianxia.shardingsphere.shardingjdbcexample.rawjdbc.masterslave;
 
+import org.apache.shardingsphere.shardingjdbc.api.yaml.YamlMasterSlaveDataSourceFactory;
+
+import javax.sql.DataSource;
+import java.io.File;
+
 /**
  * @author Nianjun Sun
  * @date 2019/9/2 10:27
  */
 public class MasterSlaveApplication {
+
+    public static void main(String... args) throws Exception {
+        DataSource dataSource = YamlMasterSlaveDataSourceFactory.createDataSource(new File(""));
+    }
 }
