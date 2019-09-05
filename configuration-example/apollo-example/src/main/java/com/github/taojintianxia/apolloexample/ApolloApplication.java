301e7174a6b4445d65ae6985da069327f6d6e0a6
sunnianjun <sunnianjun@huobi.com>
2019-09-05 13:11:52 +0800

    Add: add entity

---
+++ configuration-example/apollo-example/src/main/java/com/github/taojintianxia/apolloexample/ApolloApplication.java
@@ -0,0 +1,17 @@
+package com.github.taojintianxia.apolloexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Nianjun Sun
+ * @date 2019/9/5 09:54
+ */
+@SpringBootApplication
+public class ApolloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApolloApplication.class, args);
+    }
+}
+
