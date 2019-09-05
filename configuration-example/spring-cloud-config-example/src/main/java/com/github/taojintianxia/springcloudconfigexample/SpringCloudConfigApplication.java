6fe06591e0d49560aae82b7955176a8c04d97152
sunnianjun <sunnianjun@huobi.com>
2019-09-05 09:59:17 +0800

    Add: add spring boot dependency

---
+++ configuration-example/spring-cloud-config-example/src/main/java/com/github/taojintianxia/springcloudconfigexample/SpringCloudConfigApplication.java
@@ -0,0 +1,12 @@
+package com.github.taojintianxia.springcloudconfigexample;
+
+/**
+ * @author Nianjun Sun
+ * @date 2019/9/5 09:54
+ */
+@SpringBootApplication
+public class SpringCloudConfigApplication {
+    public static void main(String[] args) {
+        SpringCloudConfigApplication.run(NacosApplication.class, args);
+    }
+}
