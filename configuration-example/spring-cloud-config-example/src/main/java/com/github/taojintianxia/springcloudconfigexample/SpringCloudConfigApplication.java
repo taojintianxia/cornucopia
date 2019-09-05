858fac5682b9651d7e1937ff5f4c7a348eac683c
sunnianjun <sunnianjun@huobi.com>
2019-09-05 10:00:18 +0800

    Add: add application entrance

--- configuration-example/spring-cloud-config-example/src/main/java/com/github/taojintianxia/springcloudconfigexample/SpringCloudConfigApplication.java
+++ configuration-example/spring-cloud-config-example/src/main/java/com/github/taojintianxia/springcloudconfigexample/SpringCloudConfigApplication.java
@@ -1,12 +1,17 @@
 package com.github.taojintianxia.springcloudconfigexample;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author Nianjun Sun
  * @date 2019/9/5 09:54
  */
 @SpringBootApplication
 public class SpringCloudConfigApplication {
+    
     public static void main(String[] args) {
-        SpringCloudConfigApplication.run(NacosApplication.class, args);
+        SpringApplication.run(SpringCloudConfigApplication.class, args);
     }
 }
+
