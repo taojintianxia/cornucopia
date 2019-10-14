32485987749684f9e5e3bf6bff78332a52b0ce3a
sunnianjun <sunnianjun@huobi.com>
2019-10-14 18:11:05 +0800

    Add: add jdbc constant

---
+++ database-example/jdbc-example/src/main/java/com/github/taojintianxia/jdbcexample/constants/JdbcConstant.java
@@ -0,0 +1,16 @@
+package com.github.taojintianxia.jdbcexample.constants;
+
+/**
+ * @author Nianjun Sun
+ * @date 2019/10/14 18:09
+ */
+public interface JdbcConstant {
+
+    String DRIVER_CLASS_NAME = "datasource.driver-class-name";
+
+    String URL = "datasource.url";
+
+    String USERNAME = "datasource.username";
+
+    String PASSWORD = "datasource.password";
+}
