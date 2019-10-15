75b4d38721108b7685dcd4653c84348da64ecf64
sunnianjun <sunnianjun@huobi.com>
2019-10-15 19:41:17 +0800

    Add: add new impl

--- shardingsphere-example/sharding-jdbc-example/src/main/java/com/github/taojintianxia/shardingsphere/shardingjdbcexample/rawjdbc/masterslave/service/OrderService.java
+++ shardingsphere-example/sharding-jdbc-example/src/main/java/com/github/taojintianxia/shardingsphere/shardingjdbcexample/rawjdbc/masterslave/service/OrderService.java
@@ -1,6 +1,7 @@
 package com.github.taojintianxia.shardingsphere.shardingjdbcexample.rawjdbc.masterslave.service;
 
 import com.github.taojintianxia.cornucopia.shardingsphere.shardingcommon.entity.Order;
+import com.github.taojintianxia.shardingsphere.shardingjdbcexample.rawjdbc.DataSourceHolder;
 
 import java.util.List;
 
@@ -8,7 +9,7 @@ import java.util.List;
  * @author Nianjun Sun
  * @date 2019/10/15 19:20
  */
-public interface OrderService {
+public interface OrderService extends DataSourceHolder {
 
     void saveOrder(Order order);
 
--- shardingsphere-example/sharding-jdbc-example/src/main/java/com/github/taojintianxia/shardingsphere/shardingjdbcexample/rawjdbc/masterslave/service/impl/OrderServiceImpl.java
+++ shardingsphere-example/sharding-jdbc-example/src/main/java/com/github/taojintianxia/shardingsphere/shardingjdbcexample/rawjdbc/masterslave/service/impl/OrderServiceImpl.java
@@ -1,8 +1,57 @@
 package com.github.taojintianxia.shardingsphere.shardingjdbcexample.rawjdbc.masterslave.service.impl;
 
+import com.github.taojintianxia.cornucopia.shardingsphere.shardingcommon.entity.Order;
+import com.github.taojintianxia.shardingsphere.shardingjdbcexample.rawjdbc.masterslave.service.OrderService;
+import lombok.SneakyThrows;
+
+import javax.sql.DataSource;
+import java.security.SecureRandom;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.util.List;
+
 /**
  * @author Nianjun Sun
  * @date 2019/10/15 19:20
  */
-public class OrderServiceImpl {
+public class OrderServiceImpl implements OrderService {
+
+    private DataSource dataSource;
+
+    @Override
+    @SneakyThrows
+    public void saveOrder(Order order) {
+        String sql = "insert into t_order values (null," + new SecureRandom().nextInt(1000) + "," + new SecureRandom()
+                .nextInt(1000) + ", 'ready')";
+        Statement statement = getStatement();
+        statement.executeQuery(sql);
+    }
+
+    @Override
+    public void deleteOder(Long orderId) {
+
+    }
+
+    @Override
+    public List<Order> listOrders() {
+        return null;
+    }
+
+    @Override
+    public DataSource getDataSource() {
+        return dataSource;
+    }
+
+    @Override
+    public void setDataSource(DataSource datasource) {
+        this.dataSource = datasource;
+    }
+
+    @SneakyThrows
+    private Statement getStatement() {
+        Connection connection = dataSource.getConnection();
+        Statement statement = connection.createStatement();
+
+        return statement;
+    }
 }
