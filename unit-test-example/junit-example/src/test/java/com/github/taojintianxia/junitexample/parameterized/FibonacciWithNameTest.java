57db43cdc98560cbbb5faf878751392986d3e947
sunnianjun <sunnianjun@huobi.com>
2019-10-23 09:48:07 +0800

    Format: format the output

--- unit-test-example/junit-example/src/test/java/com/github/taojintianxia/junitexample/parameterized/FibonacciWithNameTest.java
+++ unit-test-example/junit-example/src/test/java/com/github/taojintianxia/junitexample/parameterized/FibonacciWithNameTest.java
@@ -15,7 +15,7 @@ import static org.junit.Assert.assertEquals;
 @RunWith(Parameterized.class)
 public class FibonacciWithNameTest {
 
-    @Parameterized.Parameters(name = "{index}: fib({0})={1}")
+    @Parameterized.Parameters(name = "{index} -> fib({0})={1}")
     public static Iterable<Object[]> data() {
         return Arrays.asList(new Object[][]{{0, 0}, {1, 1}, {2, 1}, {3, 2}, {4, 3}, {5, 5}, {6, 8}});
     }
