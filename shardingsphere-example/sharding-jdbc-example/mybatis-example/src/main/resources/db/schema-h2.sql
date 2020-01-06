29417b9d4f3e350f98dccd72fdd429a6dd9ef690
sunnianjun <sunnianjun@huobi.com>
2020-01-06 18:02:06 +0800

    Add: add sql file for init

--- shardingsphere-example/sharding-jdbc-example/mybatis-example/src/main/resources/db/schema-h2.sql
+++ shardingsphere-example/sharding-jdbc-example/mybatis-example/src/main/resources/db/schema-h2.sql
@@ -1,4 +1,6 @@
-CREATE TABLE IF NOT EXISTS `ARTICLES`(
+DROP TABLE IF EXISTS city;
+
+create table `city`(
     `id`          INTEGER PRIMARY KEY,
     `state`       VARCHAR(10) NOT NULL,
     `name`      VARCHAR(30) NOT NULL
