cbbb741cbf486d5ebf59724ef8f26294d3de9ecc
sunnianjun <sunnianjun@huobi.com>
2020-01-07 10:26:58 +0800

    Refactor: refactor the path

---
+++ database-example/mybatisplus-example/src/main/java/com/github/taojintianxia/cornucopia/database/mybatisplus/MybatisPlusApplication.java
@@ -0,0 +1,8 @@
+package com.github.taojintianxia.cornucopia.database.mybatisplus;
+
+/**
+ * @author Nianjun Sun
+ * @date 2020/1/7 10:23
+ */
+public class MybatisPlusApplication {
+}
---
+++ shardingsphere-example/sharding-jdbc-example/sharding-example/springboot-mybatisplus-example/src/main/java/com/github/taojintianxia/cornucopia/shardingsphere/shardingjdbc/sharding/mybatisplus/MybatisPlusApplication.java
@@ -0,0 +1,8 @@
+package com.github.taojintianxia.cornucopia.shardingsphere.shardingjdbc.sharding.mybatisplus;
+
+/**
+ * @author Nianjun Sun
+ * @date 2020/1/7 10:20
+ */
+public class MybatisPlusApplication {
+}
