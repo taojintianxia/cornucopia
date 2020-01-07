1fa024ecefb07d9d753b0968bcf78f766d5c161e
sunnianjun <sunnianjun@huobi.com>
2020-01-07 17:33:31 +0800

    Add: add init sql

--- database-example/mybatisplus-example/src/main/resources/db/data-mysql.sql
+++ database-example/mybatisplus-example/src/main/resources/db/data-mysql.sql
@@ -1,7 +1,8 @@
-delete from state;
+DELETE FROM user;
 
-insert into state (id, state_name, full_name) values
-(1, 'AL', 'Alabama'),
-(2, 'FL', 'Florida'),
-(3, 'LA', 'Louisiana'),
-(4, 'NY', 'New York');
+INSERT INTO user (id, name, age, email) VALUES
+(1, 'Jone', 18, 'test1@baomidou.com'),
+(2, 'Jack', 20, 'test2@baomidou.com'),
+(3, 'Tom', 28, 'test3@baomidou.com'),
+(4, 'Sandy', 21, 'test4@baomidou.com'),
+(5, 'Billie', 24, 'test5@baomidou.com');
