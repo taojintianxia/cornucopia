40192f7f81b10c9db196b743d233b250552146c8
sunnianjun <sunnianjun@huobi.com>
2020-01-07 18:17:53 +0800

    Add: add new method

--- database-example/mybatisplus-example/src/main/java/com/github/taojintianxia/cornucopia/database/mybatisplus/mapper/UserMapper.java
+++ database-example/mybatisplus-example/src/main/java/com/github/taojintianxia/cornucopia/database/mybatisplus/mapper/UserMapper.java
@@ -3,9 +3,15 @@ package com.github.taojintianxia.cornucopia.database.mybatisplus.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.github.taojintianxia.cornucopia.database.mybatisplus.entity.User;
 
+import java.util.List;
+
 /**
  * @author Nianjun Sun
  * @date 2020/1/7 17:38
  */
 public interface UserMapper extends BaseMapper<User> {
+
+    List<User> selectAll();
+
+    List<User> selectRange();
 }
