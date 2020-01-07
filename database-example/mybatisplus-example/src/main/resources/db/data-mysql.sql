79772703cbe0ad7d0ebeae6df4f8905cd5f99cd5
sunnianjun <sunnianjun@huobi.com>
2020-01-07 16:00:49 +0800

    Add: add config for mybatis plus example

---
+++ database-example/mybatisplus-example/src/main/resources/db/data-mysql.sql
@@ -0,0 +1,7 @@
+delete from state;
+
+insert into state (id, state_name, full_name) values
+(1, 'AL', 'Alabama'),
+(2, 'FL', 'Florida'),
+(3, 'LA', 'Louisiana'),
+(4, 'NY', 'New York');
---
+++ database-example/mybatisplus-example/src/main/resources/db/schema-mysql.sql
@@ -0,0 +1,9 @@
+drop table IF EXISTS state;
+
+create TABLE state
+(
+	id BIGINT(20) NOT NULL COMMENT '主键ID',
+	state_name VARCHAR(10) NULL DEFAULT NULL COMMENT 'state name',
+	full_name VARCHAR(30) NULL DEFAULT NULL COMMENT 'full name',
+	PRIMARY KEY (id)
+);
