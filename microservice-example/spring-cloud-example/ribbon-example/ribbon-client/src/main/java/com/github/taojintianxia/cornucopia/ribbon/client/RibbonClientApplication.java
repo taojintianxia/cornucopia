2ab8e72ef88ef586126bf6643f8724c26478c0c7
sunnianjun <sunnianjun@huobi.com>
2020-01-15 19:42:13 +0800

    Add: add the structure for ribbon

---
+++ microservice-example/spring-cloud-example/ribbon-example/ribbon-client/src/main/java/com/github/taojintianxia/cornucopia/ribbon/client/RibbonClientApplication.java
@@ -0,0 +1,8 @@
+package com.github.taojintianxia.cornucopia.ribbon.client;
+
+/**
+ * @author Nianjun Sun
+ * @date 2020/1/15 19:41
+ */
+public class RibbonClientApplication {
+}
---
+++ microservice-example/spring-cloud-example/ribbon-example/ribbon-server-B/src/main/java/com/github/taojintianxia/cornucopia/ribbon/serverb/RibbonServerBApplication.java
@@ -0,0 +1,5 @@
+package com.github.taojintianxia.cornucopia.ribbon.serverb;
+
+public class RibbonServerBApplication {
+
+}
