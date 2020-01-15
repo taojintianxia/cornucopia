bd07e1976e83c6a1ebd017daacbac1a0433da82e
sunnianjun <sunnianjun@huobi.com>
2020-01-15 18:28:51 +0800

    Add: add ribbon module

---
+++ microservice-example/spring-cloud-example/zuul-example/src/main/java/com/github/taojintianxia/cornucopia/zuul/ZuulApplication.java
@@ -0,0 +1,18 @@
+package com.github.taojintianxia.cornucopia.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author Nianjun Sun
+ * @date 2020/1/15 15:03
+ */
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
