0e2475bf84c62461d82b11d9865f0e8aed121036
sunnianjun <sunnianjun@huobi.com>
2020-03-12 12:09:38 +0800

    Refactor: refactor starter

--- springboot-example/customized-starter-example/cornucopia-example-spring-boot-starter/src/main/java/com/github/taojintianxia/cornucopia/springboot/customizedstarter/configuration/HelloAutoConfiguration.java
+++ springboot-example/customized-starter-example/cornucopia-example-spring-boot-starter/src/main/java/com/github/taojintianxia/cornucopia/springboot/customizedstarter/configuration/HelloAutoConfiguration.java
@@ -8,23 +8,18 @@ package com.github.taojintianxia.cornucopia.springboot.customizedstarter.configu
 import com.github.taojintianxia.cornucopia.springboot.customizedstarter.property.HelloProperty;
 import com.github.taojintianxia.cornucopia.springboot.customizedstarter.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConditionalOnClass({HelloService.class})
 @EnableConfigurationProperties(HelloProperty.class)
 public class HelloAutoConfiguration {
 
     @Autowired
     private HelloProperty helloProperty;
 
-
     @Bean
-    @ConditionalOnMissingBean(HelloService.class)
     public HelloService helloService() {
         String target = helloProperty.getTarget();
         if (target == null || target.trim() == "") {
