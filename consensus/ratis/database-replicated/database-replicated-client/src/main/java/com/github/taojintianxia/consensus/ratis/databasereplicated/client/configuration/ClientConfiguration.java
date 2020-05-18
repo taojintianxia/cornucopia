6c343122ec3094e45fb7f100133fe5427efb035d
sunnianjun <sunnianjun@huobi.com>
2020-05-18 19:52:15 +0800

    Add: add new parser for raft group

--- consensus/ratis/database-replicated/database-replicated-client/src/main/java/com/github/taojintianxia/consensus/ratis/databasereplicated/client/configuration/ClientConfiguration.java
+++ consensus/ratis/database-replicated/database-replicated-client/src/main/java/com/github/taojintianxia/consensus/ratis/databasereplicated/client/configuration/ClientConfiguration.java
@@ -2,12 +2,14 @@ package com.github.taojintianxia.consensus.ratis.databasereplicated.client.confi
 
 import lombok.Data;
 import lombok.SneakyThrows;
+import org.apache.ratis.protocol.RaftGroup;
+import org.apache.ratis.protocol.RaftGroupId;
 import org.apache.ratis.protocol.RaftPeer;
 import org.apache.ratis.protocol.RaftPeerId;
+import org.apache.ratis.thirdparty.com.google.protobuf.ByteString;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 
 /**
  * @author Nianjun Sun
@@ -18,22 +20,22 @@ public class ClientConfiguration {
 
     private List<RaftPeer> raftPeers;
 
-    public static RaftPeer[] parsePeers(String peers) {
-        return Stream.of(peers.split(",")).map(address -> {
-            String[] addressParts = address.split(":");
-            return new RaftPeer(RaftPeerId.valueOf(addressParts[0]), addressParts[1] + ":" + addressParts[2]);
-        }).toArray(RaftPeer[]::new);
-    }
+    private RaftGroup raftGroup;
 
-    @SneakyThrows
-    public ClientConfiguration swap(ClientYamlConfiguration clientYamlConfiguration) {
-        List<String> peers = clientYamlConfiguration.getPeers();
+    public List<RaftPeer> parsePeers(List<String> peers) {
         List<RaftPeer> raftPeers = new ArrayList<>(peers.size());
         peers.forEach(address -> {
             String[] addressParts = address.split(":");
             raftPeers.add(new RaftPeer(RaftPeerId.valueOf(addressParts[0]), addressParts[1] + ":" + addressParts[2]));
         });
-        this.raftPeers = raftPeers;
+
+        return raftPeers;
+    }
+
+    @SneakyThrows
+    public ClientConfiguration swap(ClientYamlConfiguration clientYamlConfiguration) {
+        this.raftPeers = parsePeers(clientYamlConfiguration.getPeers());
+        this.raftGroup = RaftGroup.valueOf(RaftGroupId.valueOf(ByteString.copyFromUtf8(clientYamlConfiguration.getRaftGroupId())), raftPeers);
         return this;
     }
 }
--- consensus/ratis/database-replicated/database-replicated-client/src/main/java/com/github/taojintianxia/consensus/ratis/databasereplicated/client/configuration/ClientYamlConfiguration.java
+++ consensus/ratis/database-replicated/database-replicated-client/src/main/java/com/github/taojintianxia/consensus/ratis/databasereplicated/client/configuration/ClientYamlConfiguration.java
@@ -12,4 +12,6 @@ import java.util.List;
 public class ClientYamlConfiguration {
 
     private List<String> peers;
+
+    private String raftGroupId;
 }
