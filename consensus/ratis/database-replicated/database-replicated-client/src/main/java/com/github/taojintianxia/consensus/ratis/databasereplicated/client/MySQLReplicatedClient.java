113a3613288144011d36e4098d78ee808c3af1ba
sunnianjun <sunnianjun@huobi.com>
2020-05-18 20:04:04 +0800

    Refactor: refactor the client code

--- consensus/ratis/database-replicated/database-replicated-client/src/main/java/com/github/taojintianxia/consensus/ratis/databasereplicated/client/MySQLReplicatedClient.java
+++ consensus/ratis/database-replicated/database-replicated-client/src/main/java/com/github/taojintianxia/consensus/ratis/databasereplicated/client/MySQLReplicatedClient.java
@@ -1,6 +1,16 @@
 package com.github.taojintianxia.consensus.ratis.databasereplicated.client;
 
 import com.github.taojintianxia.consensus.ratis.databasereplicated.client.api.ReplicatedClient;
+import com.github.taojintianxia.consensus.ratis.databasereplicated.client.configuration.ClientConfiguration;
+import com.github.taojintianxia.consensus.ratis.databasereplicated.client.util.ConfigurationUtil;
+import com.github.taojintianxia.consensus.ratis.databasereplicated.common.message.SQLMessage;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ratis.client.RaftClient;
+import org.apache.ratis.conf.Parameters;
+import org.apache.ratis.conf.RaftProperties;
+import org.apache.ratis.grpc.GrpcFactory;
+import org.apache.ratis.protocol.ClientId;
+import org.apache.ratis.protocol.RaftClientReply;
 
 import java.io.IOException;
 
@@ -8,21 +18,21 @@ import java.io.IOException;
  * @author Nianjun Sun
  * @date 2020/5/6 14:45
  */
+@Slf4j
 public class MySQLReplicatedClient implements ReplicatedClient {
 
     @Override
     public long execute(String sql) throws IOException {
-//        String raftGroupId = "demoRaftGroup123";
-//        RaftProperties raftProperties = new RaftProperties();
-//        final RaftGroup raftGroup = RaftGroup
-//                .valueOf(RaftGroupId.valueOf(ByteString.copyFromUtf8(raftGroupId)), JVMParamUtil.getPeers());
-//
-//        RaftClient.Builder builder = RaftClient.newBuilder().setProperties(raftProperties);
-//        builder.setRaftGroup(raftGroup);
-//        builder.setClientRpc(new GrpcFactory(new Parameters()).newRaftClientRpc(ClientId.randomId(), raftProperties));
-//        RaftClient client = builder.build();
-//        SQLMessage sqlMessage = new SQLMessage(sql);
-//        client.send(sqlMessage);
+        ClientConfiguration clientConfiguration = new ClientConfiguration()
+                .swap(ConfigurationUtil.loadClientYamlConfiguration());
+        RaftProperties raftProperties = new RaftProperties();
+        RaftClient.Builder builder = RaftClient.newBuilder().setProperties(raftProperties);
+        builder.setRaftGroup(clientConfiguration.getRaftGroup());
+        builder.setClientRpc(new GrpcFactory(new Parameters()).newRaftClientRpc(ClientId.randomId(), raftProperties));
+        RaftClient client = builder.build();
+        SQLMessage sqlMessage = new SQLMessage(sql);
+        RaftClientReply raftClientReply = client.send(sqlMessage);
+        log.info("raft group response : " + raftClientReply.getMessage().getContent().toString());
         return 1L;
     }
 }
