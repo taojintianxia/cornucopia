fc8b2e4d6b04b6ccc509b0703cbd6494c711d193
sunnianjun <sunnianjun@huobi.com>
2020-07-27 10:39:10 +0800

    Add: add provider for dubbo protobuf

---
+++ spring-boot/dubbo-spring-boot/dubbo-spring-boot-protobuf/dubbo-spring-boot-protobuf-provider/src/main/java/com/github/taojintianxia/springboot/dubbo/protobuf/provider/ProviderApplication.java
@@ -0,0 +1,7 @@
+package com.github.taojintianxia.springboot.dubbo.protobuf.provider;
+
+/**
+ * @author Nianjun Sun
+ */
+public class ProviderApplication {
+}
---
+++ spring-boot/dubbo-spring-boot/dubbo-spring-boot-protobuf/src/main/java/com/github/taojintianxia/springboot/dubbo/protobuff/Application.java
@@ -0,0 +1,7 @@
+package com.github.taojintianxia.springboot.dubbo.protobuff;
+
+/**
+ * @author Nianjun Sun
+ */
+public class Application {
+}
