7f72cc678a5c9e2bd6b2703a5b56dd0c48be7846
sunnianjun <sunnianjun@huobi.com>
2020-07-27 15:32:03 +0800

    Add: add simple dubbo spring boot exaple

--- spring-boot/dubbo-spring-boot/dubbo-spring-boot-protobuf/dubbo-spring-boot-protobuf-provider/src/main/java/com/github/taojintianxia/springboot/dubbo/protobuf/provider/controller/DemoController.java
+++ spring-boot/dubbo-spring-boot/dubbo-spring-boot-protobuf/dubbo-spring-boot-protobuf-provider/src/main/java/com/github/taojintianxia/springboot/dubbo/protobuf/provider/controller/DemoController.java
@@ -5,6 +5,7 @@ import org.apache.dubbo.demo.HelloReply;
 import org.apache.dubbo.demo.HelloRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -17,11 +18,9 @@ public class DemoController {
     private DemoServiceDubbo.IDemoService demoService;
 
     @GetMapping("/provider/demoService")
-    public String sayHello (){
-        HelloRequest request = HelloRequest.newBuilder().setName("Hello").build();
+    public String sayHello(@RequestParam String name) {
+        HelloRequest request = HelloRequest.newBuilder().setName(name).build();
         HelloReply helloReply = demoService.sayHello(request);
         return helloReply.getMessage();
     }
-
-
 }
