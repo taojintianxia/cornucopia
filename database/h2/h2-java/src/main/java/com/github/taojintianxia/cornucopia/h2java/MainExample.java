1e909a9b0d81aca69c7f991e76b05ac7ce4c29c2
Kane Sun <kane.sun@outlook.com>
2020-07-28 08:13:11 +0800

    Add: add the structure for h2 java

---
+++ database/h2/h2-java/src/main/java/com/github/taojintianxia/cornucopia/h2java/MainExample.java
@@ -0,0 +1,12 @@
+package com.github.taojintianxia.cornucopia.h2java;
+
+/**
+ * @author Nianjun Sun
+ * @date 2020/7/28 08:12
+ */
+public class MainExample {
+    
+    public static void main(final String... args) {
+    
+    }
+}
