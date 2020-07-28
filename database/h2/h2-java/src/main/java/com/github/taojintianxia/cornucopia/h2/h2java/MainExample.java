c81582ac7e33c414ef5db12c70e843ed6dd7a5f1
sunnianjun <sunnianjun@huobi.com>
2020-07-28 20:56:22 +0800

    Add: add a small demo for H2

--- database/h2/h2-java/src/main/java/com/github/taojintianxia/cornucopia/h2/h2java/MainExample.java
+++ database/h2/h2-java/src/main/java/com/github/taojintianxia/cornucopia/h2/h2java/MainExample.java
@@ -1,12 +1,19 @@
 package com.github.taojintianxia.cornucopia.h2.h2java;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 /**
  * @author Nianjun Sun
  * @date 2020/7/28 08:12
  */
 public class MainExample {
-    
-    public static void main(final String... args) {
-    
+
+    public static void main(final String... args) throws Exception {
+        Class.forName("org.h2.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:h2:mem:~/test;MODE=MySQL;INIT=RUNSCRIPT FROM 'classpath:scripts/create_script.sql'", "sa", "");
+        Statement statement = connection.createStatement();
+        connection.close();
     }
 }
