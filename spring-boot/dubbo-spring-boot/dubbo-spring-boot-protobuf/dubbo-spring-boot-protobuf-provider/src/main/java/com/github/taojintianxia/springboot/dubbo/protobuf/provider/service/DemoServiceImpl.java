c167f928d561afaf88163d8d2d6064f331fc349c
sunnianjun <sunnianjun@huobi.com>
2020-07-28 12:55:12 +0800

    Format: format the code

--- spring-boot/dubbo-spring-boot/dubbo-spring-boot-protobuf/dubbo-spring-boot-protobuf-provider/src/main/java/com/github/taojintianxia/springboot/dubbo/protobuf/provider/service/DemoServiceImpl.java
+++ spring-boot/dubbo-spring-boot/dubbo-spring-boot-protobuf/dubbo-spring-boot-protobuf-provider/src/main/java/com/github/taojintianxia/springboot/dubbo/protobuf/provider/service/DemoServiceImpl.java
@@ -35,10 +35,10 @@ public class DemoServiceImpl implements DemoServiceDubbo.IDemoService {
 
     @Override
     public HelloReply sayHello(HelloRequest request) {
-        logger.info("Hello " + request.getName() + ", request from consumer: " + RpcContext.getContext().getRemoteAddress());
-        return HelloReply.newBuilder()
-                .setMessage("Hello " + request.getName() + ", response from provider: "
-                        + RpcContext.getContext().getLocalAddress())
+        logger.info("Hello " + request.getName() + ", request from consumer: " + RpcContext.getContext()
+                .getRemoteAddress());
+        return HelloReply.newBuilder().setMessage(
+                "Hello " + request.getName() + ", response from provider: " + RpcContext.getContext().getLocalAddress())
                 .build();
     }
 
