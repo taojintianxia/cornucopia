dcf472d93f875a350dba43078b55ba27269a2c35
Kane Sun <kane.sun@outlook.com>
2020-08-07 07:39:01 +0800

    Refactor: refactor dubbo demo

--- spring-boot/dubbo-spring-boot/dubbo-spring-boot-simple/dubbo-spring-boot-simple-provider/src/main/java/com/github/taojintianxia/cornucopia/springboot/dubbo/simple/provider/service/DemoServiceImpl.java
+++ spring-boot/dubbo-spring-boot/dubbo-spring-boot-simple/dubbo-spring-boot-simple-provider/src/main/java/com/github/taojintianxia/cornucopia/springboot/dubbo/simple/provider/service/DemoServiceImpl.java
@@ -16,32 +16,18 @@
  */
 package com.github.taojintianxia.cornucopia.springboot.dubbo.simple.provider.service;
 
+import com.github.taojintianxia.cornucopia.springboot.dubbo.simple.api.DemoService;
 import org.apache.dubbo.config.annotation.DubboService;
-import org.apache.dubbo.demo.DemoServiceDubbo;
-import org.apache.dubbo.demo.HelloReply;
-import org.apache.dubbo.demo.HelloRequest;
-import org.apache.dubbo.rpc.RpcContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
 
-import java.util.concurrent.CompletableFuture;
-
-
-@Service
+/**
+ * @author Nianjun Sun
+ */
 @DubboService(version = "${demo.service.version}")
-public class DemoServiceImpl implements DemoServiceDubbo.IDemoService {
-    private static final Logger logger = LoggerFactory.getLogger(DemoServiceImpl.class);
-    
+public class DemoServiceImpl implements DemoService {
+
     @Override
-    public HelloReply sayHello(HelloRequest request) {
-        logger.info("Hello " + request.getName() + ", request from consumer: " + RpcContext.getContext()
-                .getRemoteAddress());
-        return HelloReply.newBuilder().setMessage("").build();
-    }
-    
-    @Override
-    public CompletableFuture<HelloReply> sayHelloAsync(HelloRequest request) {
-        return CompletableFuture.completedFuture(sayHello(request));
+    public String sayHello(String name) {
+        System.out.println("getting data from dubbo client");
+        return "Hello " + name;
     }
 }
