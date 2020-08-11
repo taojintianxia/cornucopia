eadbb13cc09cf1fab7d60fbd9f06a3c3d3e19f83
sunnianjun <sunnianjun@huobi.com>
2020-08-11 15:56:19 +0800

    Add: add demo code for shardingsphere date ranging

--- shardingsphere/date-range/src/main/java/com/github/taojintianxia/cornucopia/shardingsphere/datarange/ExampleMain.java
+++ shardingsphere/date-range/src/main/java/com/github/taojintianxia/cornucopia/shardingsphere/datarange/ExampleMain.java
@@ -1,11 +1,19 @@
 package com.github.taojintianxia.cornucopia.shardingsphere.datarange;
 
+import com.google.common.io.Resources;
+import lombok.SneakyThrows;
+import org.apache.shardingsphere.shardingjdbc.api.yaml.YamlShardingDataSourceFactory;
+
+import javax.sql.DataSource;
+import java.io.File;
+
 /**
  * @author Nianjun Sun
  */
 public class ExampleMain {
 
+    @SneakyThrows
     public static void main(final String... args) {
-        
+        DataSource dataSource = YamlShardingDataSourceFactory.createDataSource(new File(Resources.getResource("sharding-databases-tables-range.yaml").getFile()));
     }
 }
