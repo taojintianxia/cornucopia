cc30f7772fc4388ccd27f14244098d0cfa9cf80e
Nianjun Sun <kane.sun@outlook.com>
2021-07-20 18:33:39 +0800

    Fix : fix the maven dependencies issue

--- database/mybatisplus/src/test/java/com/github/taojintianxia/cornucopia/database/mybatisplus/UserTest.java
+++ database/mybatisplus/src/test/java/com/github/taojintianxia/cornucopia/database/mybatisplus/UserTest.java
@@ -2,9 +2,6 @@ package com.github.taojintianxia.cornucopia.database.mybatisplus;
 
 import com.github.taojintianxia.cornucopia.database.mybatisplus.entity.User;
 import com.github.taojintianxia.cornucopia.database.mybatisplus.mapper.UserMapper;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
