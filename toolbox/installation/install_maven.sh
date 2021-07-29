a9209f991283e65903c570031393ba4c1f2319c6
Nianjun Sun <kane.sun@outlook.com>
2021-07-29 16:24:30 +0800

    Add : add maven install script

---
+++ toolbox/installation/install_maven.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# download and install
+MAVEN_VERSION=3.6.3
+wget https://archive.apache.org/dist/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.zip
+sudo unzip apache-maven-${MAVEN_VERSION}-bin.zip -d /opt
+sudo ln -sfn /opt/apache-maven-${MAVEN_VERSION} /opt/maven
+sudo sh -c 'echo MAVEN_HOME=/opt/maven >> /etc/environment'
+sudo ln -sfn /opt/maven/bin/mvn /usr/bin/mvn
+
+# check installation
+mvn -version
+
+#cleanup
+rm apache-maven-${MAVEN_VERSION}-bin.zip
