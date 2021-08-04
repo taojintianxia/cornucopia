64cee4d2b41f5a25f8d8850f13eabd7ee5054d83
Nianjun Sun <kane.sun@outlook.com>
2021-08-04 21:47:39 +0800

    Add : add basic statistic

--- benchmark/sysbenchJ/src/main/java/com/github/taojintianxia/cornucopia/databasebenchmark/executor/ScriptBenchmarkExecutor.java
+++ benchmark/sysbenchJ/src/main/java/com/github/taojintianxia/cornucopia/databasebenchmark/executor/ScriptBenchmarkExecutor.java
@@ -5,8 +5,13 @@ import com.github.taojintianxia.cornucopia.databasebenchmark.script.ScriptHolder
 import lombok.SneakyThrows;
 
 import java.sql.PreparedStatement;
+import java.util.List;
 
 public class ScriptBenchmarkExecutor implements BenchmarkExecutor {
+
+    private List<Long> latencyList;
+
+    private int executionCount;
 
     @Override
     public void statisticize() {
@@ -16,8 +21,11 @@ public class ScriptBenchmarkExecutor implements BenchmarkExecutor {
     @Override
     @SneakyThrows
     public void execute( final ExecutorParam executorParam ) {
+        long startTime = System.currentTimeMillis();
         String sql = ScriptHolder.getInstance().getByName(executorParam.getScript()).getScriptContent();
         PreparedStatement preparedStatement = executorParam.getPreparedStatement();
         preparedStatement.execute(sql);
+        latencyList.add(System.currentTimeMillis() - startTime);
+        executionCount++;
     }
 }
