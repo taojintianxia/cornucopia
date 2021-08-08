c3f17bc88a7fd0c9ea313d67b3db0ce1eb5bc3e4
Nianjun Sun <kane.sun@outlook.com>
2021-08-08 13:49:47 +0800

    Add : add constant

--- benchmark/sysbenchJ/src/main/java/com/github/taojintianxia/cornucopia/databasebenchmark/constants/BenchmarkConstant.java
+++ benchmark/sysbenchJ/src/main/java/com/github/taojintianxia/cornucopia/databasebenchmark/constants/BenchmarkConstant.java
@@ -2,5 +2,23 @@ package com.github.taojintianxia.cornucopia.databasebenchmark.constants;
 
 public interface BenchmarkConstant {
 
+    String TABLES = "tables";
+
+    String TABLE_SIZE = "table-size";
+
+    String REPORT_INTERVAL = "report-interval";
+
+    String DB_DRIVER = "db-driver";
+
+    String PERCENTILE = "percentile";
+
     String MYSQL_HOST = "mysql-host";
+
+    String MYSQL_USER = "mysql-user";
+
+    String MYSQL_PASSWORD = "mysql-password";
+
+    String MYSQL_DB = "mysql-db";
+
+    String PGSQL_PORT = "pgsql-port";
 }
