7cff23f6970bdeb4998af3ab9a9ff9b19adfea8a
Sun Nianjun <sunnianjun@sphere-ex.com>
2021-09-29 18:12:45 +0800

    Refactor : update the test

--- shardingsphere/single-table-rule-multi-datasource/src/main/java/com/github/taojintianxia/cornucopia/ss/feature/singletablerule/EncryptTest.java
+++ shardingsphere/single-table-rule-multi-datasource/src/main/java/com/github/taojintianxia/cornucopia/ss/feature/singletablerule/EncryptTest.java
@@ -8,15 +8,16 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import javax.sql.DataSource;
 
-public class MultiDatasourceWithEncrypt {
+public class EncryptTest {
 
     public static void main( String... args ) throws SQLException, IOException {
         DataSource dataSource = YamlShardingSphereDataSourceFactory.createDataSource(getFile("/META-INF/encrypt.yaml"));
         testReadWriteEncrypt(dataSource);
+        testReadWriteSingle(dataSource);
     }
 
     private static File getFile( final String fileName) {
-        return new File(MultiDatasourceWithEncrypt.class.getResource(fileName).getFile());
+        return new File(EncryptTest.class.getResource(fileName).getFile());
     }
 
     private static void testReadWriteEncrypt(DataSource dataSource) throws SQLException {
@@ -24,14 +25,14 @@ public class MultiDatasourceWithEncrypt {
         connection.prepareStatement("DROP TABLE IF EXISTS t_encrypt;").execute();
         connection.prepareStatement("CREATE TABLE t_encrypt(encrypt_id INT, user_id VARCHAR(100), order_id VARCHAR(100), content VARCHAR(100));").execute();
         connection.prepareStatement("INSERT INTO t_encrypt(encrypt_id, user_id, order_id, content) VALUES(1, '11', '11', '11'), (2, '22', '22', '22');").execute();
-        connection.prepareStatement("UPDATE t_encrypt InitializerStrategy.SET content = '222' WHERE encrypt_id = 2;").execute();
-//        connection.prepareStatement("SELECT * FROM t_encrypt;").execute();
-//        connection.prepareStatement("DELETE FROM t_encrypt WHERE encrypt_id = 1;").execute();
-//        connection.prepareStatement("TRUNCATE TABLE t_encrypt;").execute();
-//        connection.prepareStatement("DROP TABLE t_encrypt;").execute();
+        connection.prepareStatement("UPDATE t_encrypt SET content = '222' WHERE encrypt_id = 2;").execute();
+        connection.prepareStatement("SELECT * FROM t_encrypt;").execute();
+        connection.prepareStatement("DELETE FROM t_encrypt WHERE encrypt_id = 1;").execute();
+        connection.prepareStatement("TRUNCATE TABLE t_encrypt;").execute();
+        connection.prepareStatement("DROP TABLE t_encrypt;").execute();
     }
 
-    // can not refactor this, because I will test the sql separately and have to stop for some time.
+    // can not refactor this, because I will test the sql separately and have to stop for checking.
     private static void testReadWriteSingle(DataSource dataSource) throws SQLException {
         Connection connection = dataSource.getConnection();
         connection.prepareStatement("DROP TABLE IF EXISTS t_single;").execute();
