db95d8a67aff4f320587060f3f6072ee8a75042c
Sun Nianjun <sunnianjun@sphere-ex.com>
2021-09-30 09:43:00 +0800

    Add : add test case for encrypt sharding and readwrite

--- shardingsphere/single-table-rule-multi-datasource/src/main/java/com/github/taojintianxia/cornucopia/ss/feature/singletablerule/EncryptReadWriteSplittingTest.java
+++ shardingsphere/single-table-rule-multi-datasource/src/main/java/com/github/taojintianxia/cornucopia/ss/feature/singletablerule/EncryptReadWriteSplittingTest.java
@@ -9,7 +9,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.sql.DataSource;
 
-public class EncryptTest {
+public class EncryptReadWriteSplittingTest {
 
     public static void main( String... args ) throws SQLException, IOException {
         DataSource dataSource = YamlShardingSphereDataSourceFactory.createDataSource(getFile("/META-INF/encrypt.yaml"));
@@ -24,17 +24,16 @@ public class EncryptTest {
 
     private static void testEncrypt( DataSource dataSource ) throws SQLException {
         Connection connection = dataSource.getConnection();
-        connection.prepareStatement("DROP TABLE IF EXISTS t_encrypt;").execute();
-        connection.prepareStatement("CREATE TABLE t_encrypt(encrypt_id INT, user_id VARCHAR(100), order_id VARCHAR(100), content VARCHAR(100));").execute();
-        connection.prepareStatement("INSERT INTO t_encrypt(encrypt_id, user_id, order_id, content) VALUES(1, '11', '11', '11'), (2, '22', '22', '22');").execute();
-        connection.prepareStatement("UPDATE t_encrypt SET content = '222' WHERE encrypt_id = 2;").execute();
-        connection.prepareStatement("SELECT * FROM t_encrypt;").execute();
-        connection.prepareStatement("DELETE FROM t_encrypt WHERE encrypt_id = 1;").execute();
-        connection.prepareStatement("TRUNCATE TABLE t_encrypt;").execute();
-        connection.prepareStatement("DROP TABLE t_encrypt;").execute();
+        connection.prepareStatement("DROP TABLE IF EXISTS t_test;").execute();
+        connection.prepareStatement("CREATE TABLE t_test(test_id INT, user_id VARCHAR(100), order_id VARCHAR(100), content VARCHAR(100));").execute();
+        connection.prepareStatement("ALTER TABLE t_test ADD COLUMN product_id INT;").execute();
+        connection.prepareStatement("INSERT INTO t_test(test_id, user_id, order_id, content) VALUES(1, '11', '11', '11'), (2, '22', '22', '22');").execute();
+        connection.prepareStatement("UPDATE t_test SET content = '222' WHERE test_id = 2;").execute();
+        connection.prepareStatement("SELECT * FROM t_test;").execute();
+        connection.prepareStatement("DELETE FROM t_test WHERE test_id = 1;").execute();
+        connection.prepareStatement("DROP TABLE t_test;").execute();
     }
 
-    // can not refactor this, because I will test the sql separately and have to stop for checking.
     private static void testSingle( DataSource dataSource ) throws SQLException {
         Connection connection = dataSource.getConnection();
         connection.prepareStatement("DROP TABLE IF EXISTS t_single;").execute();
@@ -50,13 +49,15 @@ public class EncryptTest {
 
     private static void testJoin( DataSource dataSource ) throws SQLException {
         Connection connection = dataSource.getConnection();
-        connection.prepareStatement("DROP TABLE IF EXISTS t_encrypt;").execute();
-        connection.prepareStatement("CREATE TABLE t_encrypt(encrypt_id INT, user_id VARCHAR(100), order_id VARCHAR(100), content VARCHAR(100));").execute();
-        connection.prepareStatement("INSERT INTO t_encrypt(encrypt_id, user_id, order_id, content) VALUES(1, '11', '11', '11'), (2, '22', '22', '22');").execute();
+        connection.prepareStatement("DROP TABLE IF EXISTS t_test;").execute();
         connection.prepareStatement("DROP TABLE IF EXISTS t_single;").execute();
+        connection.prepareStatement("CREATE TABLE t_test(test_id INT, user_id VARCHAR(100), order_id VARCHAR(100), content VARCHAR(100));").execute();
+        connection.prepareStatement("ALTER TABLE t_test ADD COLUMN product_id INT;").execute();
+        connection.prepareStatement("INSERT INTO t_test(test_id, user_id, order_id, content) VALUES(1, '11', '11', '11'), (2, '22', '22', '22');").execute();
         connection.prepareStatement("CREATE TABLE t_single(single_id INT, content VARCHAR(100));").execute();
+        connection.prepareStatement("ALTER TABLE t_single ADD COLUMN product_id INT;").execute();
         connection.prepareStatement("INSERT INTO t_single(single_id, content) VALUES(1, '11'), (2, '22');").execute();
-        ResultSet resultSet = connection.prepareStatement("SELECT * FROM t_encrypt e INNER JOIN t_single s ON e.encrypt_id = s.single_id WHERE e.encrypt_id = 1;").executeQuery();
+        ResultSet resultSet = connection.prepareStatement("SELECT * FROM t_test t INNER JOIN t_single s ON t.test_id = s.single_id WHERE t.test_id = 1;").executeQuery();
         while (resultSet.next()) {
             System.out.println("the content is " + resultSet.getString("content"));
         }
