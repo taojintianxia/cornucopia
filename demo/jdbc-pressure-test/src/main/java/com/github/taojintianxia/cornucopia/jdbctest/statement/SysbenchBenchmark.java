fcddc93f1bb4916ae73ef92b1c35c53bc16c8162
孙念君 Nianjun Sun <sunnianjun@apache.org>
2023-02-27 19:44:40 +0800

    Refactor : remove the old demo

--- demo/jdbc-pressure-test/src/main/java/com/github/taojintianxia/cornucopia/jdbctest/statement/SysbenchBenchmark.java
+++
@@ -1,8 +0,0 @@
-package com.github.taojintianxia.cornucopia.jdbctest.statement;
-
-import java.sql.SQLException;
-
-public interface SysbenchBenchmark {
-
-    void execute() throws SQLException;
-}
--- demo/springboot-mvc/src/main/resources/sql/1.create_table.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE IF NOT EXISTS sphereex_poc;
-use sphereex_poc;
-DROP TABLE IF EXISTS t_order;
-CREATE TABLE t_order (order_id INT NOT NULL, user_id INT NOT NULL, status VARCHAR(45) NULL, PRIMARY KEY (order_id));
