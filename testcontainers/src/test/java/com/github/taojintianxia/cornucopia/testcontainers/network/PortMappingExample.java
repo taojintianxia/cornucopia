a505a85cd40e4e429e27901597c0136461c21164
孙念君 Nianjun Sun <sunnianjun@apache.org>
2024-06-07 11:52:43 +0800

    Refactor : update the testcontainer example

--- testcontainers/src/test/java/com/github/taojintianxia/cornucopia/testcontainers/network/PortMappingExample.java
+++ testcontainers/src/test/java/com/github/taojintianxia/cornucopia/testcontainers/network/PortMappingExample.java
@@ -1,8 +1,10 @@
 package com.github.taojintianxia.cornucopia.testcontainers.network;
 
 import com.github.taojintianxia.cornucopia.testcontainers.mysql.container.MySQLContainer;
+import org.testcontainers.shaded.org.awaitility.Awaitility;
 
-import java.util.ArrayList;
+import java.time.Duration;
+import java.util.LinkedList;
 import java.util.List;
 
 public final class PortMappingExample {
@@ -10,12 +12,11 @@ public final class PortMappingExample {
     public static void main(String... args) throws Exception {
         try (MySQLContainer container = new MySQLContainer("mysql:5.7.38").withExposedPorts(3306)) {
             container.addEnv("MYSQL_ROOT_PASSWORD", "root");
-            List<String> portBindings = new ArrayList<>();
+            List<String> portBindings = new LinkedList<>();
             portBindings.add("53306:3306");
             container.setPortBindings(portBindings);
             container.start();
-//            Awaitility.await().atMost(Duration.ofMinutes(10));
-            Thread.sleep(1000L * 60 * 10);
+            Awaitility.await().pollDelay(Duration.ofMinutes(10)).timeout(Duration.ofMinutes(15)).until(() -> true);
         }
     }
 }
